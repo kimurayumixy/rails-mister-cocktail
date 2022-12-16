04a9326265a6dc1163e372471cff82c65a8bccc5
Yumi Kimura <kimurayumixy@gmail.com>
2022-12-16 16:49:22 +0900

    seed organized

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,13 @@ Ingredient.destroy_all
 puts "Destroying all cocktail..."
 Cocktail.destroy_all
 
-Cocktail.create!(
-  name: "Gin tonic"
-)
-
-Cocktail.create!(
-  name: "Mojito"
-)
-
-Cocktail.create!(
-  name: "Whiskey Sour"
-)
-
-Ingredient.create!(
-  name: 'Gin'
-)
-
-Ingredient.create!(
-  name: 'Whiskey'
-)
-
-Ingredient.create!(
-  name: 'Ram'
-)
-
-Ingredient.create!(
-  name: 'Lemon'
-)
+Cocktail.create!(name: "Gin tonic")
+Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Whiskey Sour")
+Ingredient.create!(name: 'Gin')
+Ingredient.create!(name: 'Whiskey')
+Ingredient.create!(name: 'Ram')
+Ingredient.create!(name: 'Lemon')
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
