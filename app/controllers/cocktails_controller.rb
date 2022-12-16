e2d82c8b41334574594cd0624336ff69b310f462
Yumi Kimura <kimurayumixy@gmail.com>
2022-12-16 17:38:17 +0900

    create new method

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,5 +3,20 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
+  def new
+    @cocktail = Cocktail.new
+  end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
 end
